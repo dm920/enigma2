2b2bed0c4a2a8a9610e62e6e35dfba3bb3881b46
dm920 <152501796+dm920@users.noreply.github.com>
2023-12-26 21:31:07 +0100

    Add files via upload

--- lib/python/Tools/HardwareInfo.py
+++ lib/python/Tools/HardwareInfo.py
@@ -1,5 +1,4 @@
 from Tools.Directories import SCOPE_SKIN, resolveFilename
-from Components.SystemInfo import BoxInfo
 
 hw_info = None
 
@@ -11,7 +10,6 @@ class HardwareInfo:
 	device_version = ""
 	device_revision = ""
 	device_hdmi = False
-
 
 	def __init__(self):
 		global hw_info
@@ -39,11 +37,42 @@ class HardwareInfo:
 			pass
 
 		# Brandname ... bit odd, but history prevails
-		self.device_brandname = BoxInfo.getItem("displaybrand")
+		try:
+			self.device_brandname = open("/proc/stb/info/brandname").read().strip()
+		except:
+			pass
+
+		# Model
+		for line in open((resolveFilename(SCOPE_SKIN, 'hw_info/hw_info.cfg')), 'r'):
+			if not line.startswith('#') and not line.isspace():
+				l = line.strip().replace('\t', ' ')
+				if ' ' in l:
+					infoFname, prefix = l.split()
+				else:
+					infoFname = l
+					prefix = ""
+				try:
+					self.device_model = prefix + open("/proc/stb/info/" + infoFname).read().strip()
+					break
+				except:
+					pass
 
 		# standard values
-		self.device_model = self.machine_name = BoxInfo.getItem("machine")
-		self.device_hw = BoxInfo.getItem("displaymodel")
+		self.device_model = self.device_model or self.device_name
+		self.device_hw = self.device_model
+		self.machine_name = self.device_model
+
+		# custom overrides for specific receivers
+		if self.device_model.startswith(("et9", "et4", "et5", "et6", "et7")):
+			self.machine_name = "%sx00" % self.device_model[:3]
+		elif self.device_model == "et11000":
+			self.machine_name = "et1x000"
+		elif self.device_brandname == "Zgemma":
+			if self.device_model and self.device_name and "H9Twin" in self.device_model and "combo" in self.device_name:
+				self.device_model = self.device_model.lower().replace(" ", "")
+			else:
+				self.device_model = self.device_name
+			self.machine_name = self.device_name
 
 		if self.device_revision:
 			self.device_string = "%s (%s-%s)" % (self.device_hw, self.device_revision, self.device_version)
@@ -52,32 +81,28 @@ class HardwareInfo:
 		else:
 			self.device_string = self.device_hw
 
-		self.device_hdmi = BoxInfo.getItem('hdmi')
+		# only some early DMM boxes do not have HDMI hardware
+		self.device_hdmi = self.device_model not in ("dm800", "dm8000")
 
+		print("Detected: " + self.get_device_string())
 
 	def get_device_name(self):
 		return hw_info.device_name
 
-
 	def get_device_model(self):
 		return hw_info.device_model
-
 
 	def get_device_version(self):
 		return hw_info.device_version
 
-
 	def get_device_revision(self):
 		return hw_info.device_revision
-
 
 	def get_device_string(self):
 		return hw_info.device_string
 
-
 	def get_machine_name(self):
 		return hw_info.machine_name
-
 
 	def has_hdmi(self):
 		return hw_info.device_hdmi
