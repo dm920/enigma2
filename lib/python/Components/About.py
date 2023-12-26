826674a6f6d82ad5451a43b7a0d9f8f604f8cb61
dm920 <152501796+dm920@users.noreply.github.com>
2023-12-26 20:08:41 +0100

    Add files via upload

--- lib/python/Components/About.py
+++ lib/python/Components/About.py
@@ -3,8 +3,8 @@ import os
 import time
 import re
 from Tools.HardwareInfo import HardwareInfo
+from Components.SystemInfo import BoxInfo
 from sys import maxsize, modules, version_info
-from subprocess import PIPE, Popen
 
 
 def getVersionString():
@@ -29,14 +29,11 @@ def getFlashDateString():
 	return _("unknown")
 
 
+def returndate(date):
+    return "%s-%s-%s" % (date[:4], date[4:6], date[6:8])
+
 def getBuildDateString():
-	try:
-		if os.path.isfile('/etc/version'):
-			version = open("/etc/version", "r").read()
-			return "%s-%s-%s" % (version[:4], version[4:6], version[6:8])
-	except:
-		pass
-	return _("unknown")
+	return returndate(BoxInfo.getItem("compiledate"))
 
 
 def getUpdateDateString():
@@ -44,7 +41,7 @@ def getUpdateDateString():
 		from glob import glob
 		build = [x.split("-")[-2:-1][0][-8:] for x in open(glob("/var/lib/opkg/info/openpli-bootlogo.control")[0], "r") if x.startswith("Version:")][0]
 		if build.isdigit():
-			return "%s-%s-%s" % (build[:4], build[4:6], build[6:])
+			return returndate(build)
 	except:
 		pass
 	return _("unknown")
@@ -52,9 +49,14 @@ def getUpdateDateString():
 
 def getEnigmaVersionString():
 	import enigma
-	enigma_version = enigma.getEnigmaVersionString()
+	enigma_version = enigma.getEnigmaVersionString().title()
 	if '-(no branch)' in enigma_version:
 		enigma_version = enigma_version[:-12]
+	enigma_version = enigma_version.rsplit("-", enigma_version.count("-") - 2)
+	if len(enigma_version) == 3:
+		enigma_version = enigma_version[0] + " (" + enigma_version[2] + "-" + enigma_version[1] + ")"
+	else:
+		enigma_version = enigma_version[0] + " (" + enigma_version[1] + ")"
 	return enigma_version
 
 
@@ -77,10 +79,7 @@ def getffmpegVersionString():
 
 
 def getKernelVersionString():
-	try:
-		return open("/proc/version", "r").read().split(' ', 4)[2].split('-', 2)[0]
-	except:
-		return _("unknown")
+	return BoxInfo.getItem("kernel")
 
 
 def getHardwareTypeString():
@@ -88,11 +87,7 @@ def getHardwareTypeString():
 
 
 def getImageTypeString():
-	try:
-		image_type = open("/etc/issue").readlines()[-2].strip()[:-6]
-		return image_type.capitalize().replace("develop", "Nightly Build")
-	except:
-		return _("undefined")
+	return BoxInfo.getItem("oe").title()
 
 
 def getCPUInfoString():
@@ -151,10 +146,10 @@ def getDriverInstalledDate():
 	try:
 		from glob import glob
 		try:
-			driver = [x.split("-")[-2] for x in open(glob("/var/lib/opkg/info/*-dvb-modules-*.control")[0], "r") if x.startswith("Version:")][0]
+			driver = [x.split("-") for x in open(glob("/var/lib/opkg/info/*-dvb-modules-*.control")[0], "r") if x.startswith("Version:")][0]
 			if len(driver) == 2:
 				driver = driver[0].split('+')
-			return "%s-%s-%s" % (driver[:4], driver[4:6], driver[6:])
+			return "%s-%s-%s" % (driver[1][:4], driver[1][4:6], driver[1][6:])
 		except:
 			try:
 				driver = [x.split("Version:") for x in open(glob("/var/lib/opkg/info/*-dvb-proxy-*.control")[0], "r") if x.startswith("Version:")][0]
@@ -221,15 +216,6 @@ def getBoxUptime():
 	except:
 		return '-'
 
-def getOpenSSLVersion():
-	process = Popen(("/usr/bin/openssl", "version"), stdout=PIPE, stderr=PIPE, universal_newlines=True)
-	stdout, stderr = process.communicate()
-	if process.returncode == 0:
-		data = stdout.strip().split()
-		if len(data) > 1 and data[0] == "OpenSSL":
-			return data[1]
-	print("[About] Get OpenSSL version failed.")
-	return _("Unknown")
 
 # For modules that do "from About import about"
 about = modules[__name__]
